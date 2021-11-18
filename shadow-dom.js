febebd2c10c995e9151a8921203b0e0ee123e4c5
Nicholas Hoyt <nhoyt@illinois.edu>
2021-11-18 13:38:45 -0600

    Initial commit

---
+++ shadow-dom.js
@@ -0,0 +1,25 @@
+/*
+*   shadow-dom.js
+*/
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <div>
+    <h1>H1 in Shadow DOM</h1>
+    <h2>H2 in Shadow DOM</h2>
+    <slot name="slot-one"></slot>
+    <slot name="slot-two"></slot>
+    <slot></slot>
+    <h3>H3 in Shadow DOM</h3>
+  </div>
+`;
+
+class ShadowDOMTest extends HTMLElement {
+  constructor () {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define('shadow-dom-test', ShadowDOMTest);
