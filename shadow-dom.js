a0cdfa4c60d21870db169586c7675c9be502a5b7
Nicholas Hoyt <nhoyt@illinois.edu>
2021-11-19 07:59:07 -0600

    Update shadow-dom.js: Add fallback content for slot

--- shadow-dom.js
+++ shadow-dom.js
@@ -8,7 +8,7 @@ template.innerHTML = `
     <h1>H1 in Shadow DOM</h1>
     <h2>H2 in Shadow DOM</h2>
     <slot name="slot-one"></slot>
-    <slot name="slot-two"></slot>
+    <slot name="slot-two"><h2>H2 in Shadow DOM: Slot Two with Fallback Content</h2></slot>
     <slot></slot>
     <h3>H3 in Shadow DOM</h3>
   </div>
