c3acf3088f0147a1657cdc388ec52701a0f28d8d
Nicholas Hoyt <nhoyt@illinois.edu>
2022-03-21 20:18:40 -0500

    Initial commit

---
+++ debug.js
@@ -0,0 +1,51 @@
+/* debug.js */
+
+export default class DebugLogging {
+  constructor (...args) {
+    this._flag = true;
+    this._label = 'debug';
+
+    // Constructor may be called with zero, one or two arguments. If two
+    // arguments, they can be in any order: one is assumed to be the boolean
+    // value for '_flag' and the other one the string value for '_label'.
+    for (const arg of args) {
+      switch (typeof arg) {
+        case 'boolean':
+          this._flag = arg;
+          break;
+        case 'string':
+          this._label = arg;
+          break;
+      }
+    }
+  }
+
+  get flag () { return this._flag; }
+
+  set flag (value) {
+    if (typeof value === 'boolean') {
+      this._flag = value;
+    }
+  }
+
+  set label (value) {
+    if (typeof value === 'string') {
+      this._label = value;
+    }
+  }
+
+  log (message, spaceAbove) {
+    const newline = spaceAbove ? '\n' : '';
+    console.log(`${newline}[${this._label}] ${message}`);
+  }
+
+  tag (elem, spaceAbove) {
+    if (elem && elem.tagName) {
+      this.log(`[${elem.tagName}]: ${elem.textContent.trim().substring(0, 20).trim()}`, spaceAbove);
+    }
+  }
+
+  separator (spaceAbove) {
+    this.log('-----------------------------', spaceAbove);
+  }
+}
