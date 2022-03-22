b513f0e6dd6924214c0d82d30851cba6528c865a
Nicholas Hoyt <nhoyt@illinois.edu>
2022-03-22 09:36:53 -0500

    Update debug.js: add usage comments; label getter

--- debug.js
+++ debug.js
@@ -1,9 +1,35 @@
-/* debug.js */
+/*
+*   debug.js
+*
+*   Usage
+*     import DebugLogging from './debug.js';
+*     const debug = new DebugLogging('myLabel', true); // e.g. 'myModule'
+*     ...
+*     if (debug.flag) debug.log('myMessage');
+*
+*   Notes
+*     new DebugLogging() - calling the constructor with no arguments results
+*                   in debug.flag set to false and debug.label set to 'debug';
+*                   constructor accepts 0, 1 or 2 arguments in any order
+*                   @param flag {boolean} optional - sets debug.flag
+*                   @param label {string} optional - sets debug.label
+*   Properties
+*     debug.flag    {boolean} allows you to switch debug logging on or off;
+*                   default value is false
+*     debug.label   {string} rendered as a prefix to each log message;
+*                   default value is 'debug'
+*   Methods
+*     debug.log     @param message {string}
+*                   @param spaceAbove {boolean} optional
+*     debug.tag     @param node {DOM node reference} - usually an HTMLElement
+*                   @param spaceAbove {boolean} optional
+*     debug.separator @param spaceAbove {boolean} optional
+*/
 
 export default class DebugLogging {
   constructor (...args) {
     // Default values for cases where fewer than two arguments are provided
-    this._flag = true;
+    this._flag = false;
     this._label = 'debug';
 
     // The constructor may be called with zero, one or two arguments. If two
@@ -30,6 +56,8 @@ export default class DebugLogging {
       this._flag = value;
     }
   }
+
+  get label () { return this._label; }
 
   set label (value) {
     if (typeof value === 'string') {
